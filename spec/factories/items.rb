98f4582b7101ec01b0164b653f81e2305bb226a5
rui1100 <yusuke.6002.1150@gmail.com>
2023-12-09 13:03:12 +0900

    商品出品機能_モデルテスト_1

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    name { Faker::Commerce.product_name }
+    description { Faker::Lorem.paragraph }
+    category { Category.all.sample }
+    status { Status.all.sample }
+    postage { Postage.all.sample }
+    prefecture { Prefecture.all.sample }
+    day { Day.all.sample }
+    price { Faker::Commerce.price(range: 300..9999999) }
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品' do
+    it '商品画像が空では登録できない' do
+    end
+    it '商品名が空では登録できない' do
+      @item.name = ''
+      @item.valid?
+      expect(@user.errors.full_messages).to include("Name can't be blank")
+    end
+    it '商品の説明が空では登録できない' do
+      @item.description = ''
+      @item.valid?
+      expect(@user.errors.full_messages).to include("Description can't be blank")
+    end
+    # 商品の詳細
+    it 'カテゴリーが「---」では登録できない' do
+    end
+    it '商品の状態が「---」では登録できない' do
+    end
+    # 配送について
+    it '配送料の負担が「---」では登録できない' do
+    end
+    it '発送元の地域が「---」では登録できない' do
+    end
+    it '発送までの日数が「---」では登録できない' do
+    end
+    # 販売価格
+    it '価格が空では登録できない' do
+    end
+    it '価格が¥300未満では登録できない' do
+    end
 end
