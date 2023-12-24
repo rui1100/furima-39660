4cc4c7870d6e9ee05ee1e2887efb08098444c72b
rui1100 <yusuke.6002.1150@gmail.com>
2023-12-24 12:26:01 +0900

    商品購入機能_モデル編集_2

--- db/migrate/20231224030438_create_deliveries.rb
+++ db/migrate/20231224030438_create_deliveries.rb
@@ -3,11 +3,11 @@ class CreateDeliveries < ActiveRecord::Migration[7.0]
     create_table :deliveries do |t|
       t.string       :post_code,     null: false
       t.integer      :prefecture_id, null: false
-      t.city         :string,        null: false
-      t.address      :string,        null: false
-      t.building     :string
-      t.phone_number :string,        null: false
-      t.order        :references,    null: false, foreign_keys: true
+      t.string       :city,          null: false
+      t.string       :address,       null: false
+      t.string       :building
+      t.string       :phone_number,  null: false
+      t.references   :order,         null: false, foreign_keys: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_21_093756) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_24_030438) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -47,6 +47,19 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_21_093756) do
   create_table "days", charset: "utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "deliveries", charset: "utf8", force: :cascade do |t|
+    t.string "post_code", null: false
+    t.integer "prefecture_id", null: false
+    t.string "city", null: false
+    t.string "address", null: false
+    t.string "building"
+    t.string "phone_number", null: false
+    t.bigint "order_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_deliveries_on_order_id"
   end
 
   create_table "items", charset: "utf8", force: :cascade do |t|
