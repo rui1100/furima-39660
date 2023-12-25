83bee8b0770b5c8675c6876dc7299ecccb6996f5
rui1100 <yusuke.6002.1150@gmail.com>
2023-12-25 13:38:49 +0900

    商品購入機能_エラーメッセージ表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,8 +11,8 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @order_form = OrderForm.new(order_params)
-
     if @order_form.valid?
       @order_form.save
       redirect_to root_path
