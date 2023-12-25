5001a9c973d9ce4aab8bcb1812e362e0c55653a2
rui1100 <yusuke.6002.1150@gmail.com>
2023-12-25 21:07:02 +0900

    商品購入機能_モデルテスト_3

--- spec/factories/order_forms.rb
+++ spec/factories/order_forms.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :order_form do
     post_code { '123-4567' }
-    prefecture_id { Prefecture.where.not(id: 1).sample }
+    prefecture_id { 10 }
     city { 'Sample City' }
     address { '123 Sample Street' }
     building { 'Sample Building' }
-    phone_number { '090012345678' }
+    phone_number { '09012345678' }
 
     association :user
     association :item
--- spec/models/order_form_spec.rb
+++ spec/models/order_form_spec.rb
@@ -10,41 +10,75 @@ RSpec.describe OrderForm, type: :model do
   describe '購入機能' do
     context '購入できる場合' do
       it '正しい情報で購入できること' do
+        expect(@order_form).to be_valid
       end
     end
 
     context '購入できない場合' do
       it '郵便番号が必須であること' do
+        @order_form.post_code = nil
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("Post code can't be blank")
       end
 
       it '郵便番号は、「3桁ハイフン4桁」の半角文字列のみ保存可能なこと' do
+        @order_form.post_code = '1234567'
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("Post code is invalid")
       end
 
       it '都道府県が必須であること' do
+        @order_form.prefecture_id = 1
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("Prefecture must be other than 1")
       end
 
       it '市区町村が必須であること' do
+        @order_form.city = nil
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("City can't be blank")
       end
 
       it '番地が必須であること' do
+        @order_form.address = nil
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("Address can't be blank")
       end
 
       it '電話番号が必須であること' do
+        @order_form.phone_number = nil
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("Phone number can't be blank")
       end
 
-      it '電話番号は9桁以上であること' do
+      it '電話番号は10桁以上であること' do
+        @order_form.phone_number = '123456789'
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("Phone number is invalid")
       end
 
       it '電話番号は12桁未満であること' do
+        @order_form.phone_number = '123456789012'
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("Phone number is invalid")
       end
 
       it '電話番号は半角数値のみであること' do
+        @order_form.phone_number = '090abcdefgh'
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("Phone number is invalid")
       end
 
       it 'ユーザー情報が紐づいていること' do
+        @order_form.user_id = nil
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("User can't be blank")
       end
 
       it '商品情報が紐づいていること' do
+        @order_form.item_id = nil
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("Item can't be blank")
       end
     end
   end
