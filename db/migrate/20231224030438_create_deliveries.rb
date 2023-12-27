d2527848afa0c7f8aae053e221e4ad5a3e4adfe2
rui1100 <yusuke.6002.1150@gmail.com>
2023-12-27 16:27:41 +0900

    商品購入機能_修正_3

--- db/migrate/20231224030438_create_deliveries.rb
+++ db/migrate/20231224030438_create_deliveries.rb
@@ -7,7 +7,7 @@ class CreateDeliveries < ActiveRecord::Migration[7.0]
       t.string       :address,       null: false
       t.string       :building
       t.string       :phone_number,  null: false
-      t.references   :order,         null: false, foreign_keys: true
+      t.references   :order,         null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -121,6 +121,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_24_030438) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "deliveries", "orders"
   add_foreign_key "items", "users"
   add_foreign_key "orders", "items"
   add_foreign_key "orders", "users"
